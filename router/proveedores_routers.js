a7ec5ea7495369c53fee987919351c7c0bf6d0c5
fran7304 <73048102@certus.edu.pe>
2021-11-22 00:07:06 -0500

    provee

---
+++ router/proveedores_routers.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const router = express.Router()
+const PROVEEDORES = require("../models/esquema_proveedores")
+
+router.get("/",async (req,res)=>{
+    try {
+        const arrayproveedoresdb = await PROVEEDORES.find()
+        console.log(arrayproveedoresdb)
+        res.render("proveedores",{
+            arrayproveedores:arrayproveedoresdb
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//exportar modulo
+module.exports = router
